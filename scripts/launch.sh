dc7e70e39da0cb4782e352ddcbe37852584b279e
namlev <mikipatkowski@protonmail.com>
2024-08-07 11:04:25 +0200

    Change launch script

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -3,11 +3,31 @@ killall waybar
 pkill waybar
 sleep 0.2 
 
-# Get the number of active monitors
-ACTIVE_MONITORS=$(swaymsg -t get_outputs | jq '.[] | select(.active) | .name' | wc -l)
+GAP_FILE="$HOME/.config/sway/current_gap"
 
-if [ "$ACTIVE_MONITORS" -eq 2 ]; then
-  waybar -c ~/.config/waybar/waybar-two-monitors.jsonc -s ~/.config/waybar/styles.css &
+if [ ! -f "$GAP_FILE" ]; then
+    echo 0 > "$GAP_FILE"
+fi
+
+current_gap=$(cat "$GAP_FILE")
+if [ "$current_gap" -eq 10 ]; then
+    new_gap=0
 else
-  waybar -c ~/.config/waybar/waybar-one-monitor.jsonc -s ~/.config/waybar/styles.css &
+    new_gap=10
+fi
+
+echo "$new_gap" > "$GAP_FILE"
+
+swaymsg gaps inner all set "$new_gap"
+
+# if gap == 10, show waybar
+if [ "$new_gap" -eq 10 ]; then
+    # Get the number of active monitors
+    ACTIVE_MONITORS=$(swaymsg -t get_outputs | jq '.[] | select(.active) | .name' | wc -l)
+
+    if [ "$ACTIVE_MONITORS" -eq 2 ]; then
+        waybar -c ~/.config/waybar/waybar-two-monitors.jsonc -s ~/.config/waybar/styles.css &
+    else
+        waybar -c ~/.config/waybar/waybar-one-monitor.jsonc -s ~/.config/waybar/styles.css &
+    fi
 fi
